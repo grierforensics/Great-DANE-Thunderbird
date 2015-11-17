7a83a8b4e8ec68c72cfd40613976f5c0da1ea728
dguiley <dathan@powerup.cc>
2015-11-17 12:34:38 -0700

    - attempt to load cert on email authoring - still not working. - partial email address scrubbing and handling - logging improvements - removed hacked in emails

--- content/overlay.js
+++ content/overlay.js
@@ -1,41 +1,106 @@
 // (C) Copyright 2015 Grier Forensics.  All rights reserved.
-Components.utils.import("resource:///modules/gloda/index_msg.js");
-Components.utils.import("resource:///modules/gloda/mimemsg.js");
-Components.utils.import("resource://gre/modules/Services.jsm");
-Components.utils.import("resource://gre/modules/NetUtil.jsm");
 
-Components.utils.import("resource://greatdane/greatdane.js");
+const Ci = Components.interfaces;
+const Cc = Components.classes;
+const Cu = Cu;
 
+Cu.import("resource:///modules/gloda/index_msg.js");
+Cu.import("resource:///modules/gloda/mimemsg.js");
+Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/NetUtil.jsm");
+Cu.import("resource://greatdane/greatdane.js");
 
 var console = Components.classes["@mozilla.org/consoleservice;1"].getService(Components.interfaces.nsIConsoleService);
+var emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+var session = {};
 
 var GreatdaneOverlay = {
+
   click: function () {
     if (document.readyState === "complete") {
-      //this.openTab("chrome://greatdane/content/index.html");
-
-      this.processEmailAddresses(['danetest@had-pilot.biz']);
+      var emailAddresses = this.getEmailsForCurrentMessage();
+      for each(var emailAddress in emailAddresses) {
+        this.processEmailAddress(emailAddress);
+      }
+      //this.openTab("chrome://greatdane/content/index.html"); //index shows cert output
     }
   },
 
-  processEmailAddresses: function (emailAddresses) {
-    //var obj = Greatdane.getEmailsForCurrentMessage();
-    //console.logStringMessage("getEmailsForCurrentMessage:"+JSON.stringify(obj));
+  processEmailAddress: function (emailAddress) {
+    var scrubbed = this.scrubEmailAddress(emailAddress);
+    if (!scrubbed || scrubbed in session) {
+      return;//bad email or already tried
+    }
 
-    Greatdane.getCertsForEmailAddresses(emailAddresses, function (certsForEmails) {
-      for each(var entry in certsForEmails) {
-        console.logStringMessage("adding " + entry.certs.length + " certs for email:" + entry.email);
-        for each(var cert in entry.certs) {
-          Greatdane.addCertificate(cert, 'C,c,c');
-        }
+    Greatdane.getCertsForEmailAddress(scrubbed, function (certs) {
+      session[scrubbed] = true;
+      console.logStringMessage("DANE lookup success. Adding/updating " + certs.length + " certs for email=" + scrubbed);
+      for each(var cert in certs) {
+        Greatdane.addCertificate(cert, 'C,c,c');
       }
+    }, function (responseText) {
+      session[scrubbed] = false;
+      console.logStringMessage("DANE lookup ERROR. email=" + scrubbed + " result=" + responseText);//debug
     });
+  },
+
+  processPartialEmailAddress: function (emailAddresses) {
+    var scrubbed = this.scrubEmailAddress(emailAddress);
+    if (!scrubbed || scrubbed in session) {
+      return;//bad email or already tried
+    }
+
+    Greatdane.getCertsForEmailAddress(scrubbed, function (certs) {
+      session[scrubbed] = true;
+      console.logStringMessage("DANE lookup success. Adding/updating " + certs.length + " certs for email=" + emailAddress);
+      for each(var cert in certs) {
+        Greatdane.addCertificate(cert, 'C,c,c');
+      }
+    }, function (responseText) {
+      session[scrubbed] = false;
+      /*remove this*/
+      console.logStringMessage("partial DANE lookup ERROR. email=" + emailAddress + " result=" + responseText);//debug
+    });
+  },
+
+  processPartialTextbox: function (element) {
+    var value = element.inputField.value;
+    if (!value)
+      return;
+
+    for each(var partial in value.split(',')) {
+      this.processPartialEmailAddress(partial);
+    }
+  },
+
+  getEmailsForCurrentMessage: function () {
+    // First we get msgHdr, a nsIMsgDbHdr
+    let mainWindow = this.getMail3Pane();
+    let tabmail = mainWindow.document.getElementById("tabmail");
+    // We might not always have a selected message, so check first
+    let msgHdr = mainWindow.gFolderDisplay.selectedMessage;
+
+    return [msgHdr.author]; //todo: return other fields
+  },
+
+  getMail3Pane: function () {
+    return Cc["@mozilla.org/appshell/window-mediator;1"]
+        .getService(Ci.nsIWindowMediator)
+        .getMostRecentWindow("mail:3pane");
+  },
+
+  scrubEmailAddress: function (emailAddress) {
+    if (!emailAddress || emailAddress.length <= 6)
+      return null;
+
+    var result = emailAddress.replace(/.*?</, "").replace(/>.*?/, "").trim();
+    return emailRegex.test(result) ? result : null;
   },
 
   openTab: function (page) {
     document.getElementById("tabmail").openTab("chromeTab", {chromePage: page});
   }
-};
+}
 
 window.addEventListener("load", function _overlay_eventListener() {
   // Fixup.
@@ -46,7 +111,7 @@ window.addEventListener('load', function _setup_greatdate_eventlisteners() {
   var newMailListener = {
     msgAdded: function (msgHdr) {
       if (!msgHdr.isRead) {
-        GreatdaneOverlay.processEmailAddresses([msgHdr.author]);
+        GreatdaneOverlay.processEmailAddress(msgHdr.author);
       }
     }
   };
--- modules/greatdane.js
+++ modules/greatdane.js
@@ -7,11 +7,6 @@ var EXPORTED_SYMBOLS = ['Greatdane']
 const Ci = Components.interfaces;
 const Cc = Components.classes;
 const Cu = Components.utils;
-const Cr = Components.results;
-
-const hostAndPort = 'localhost:7777';
-const CERT_TRUST = "C,C,C";
-//const hostAndPort = 'dst.grierforensics.com';
 
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource://gre/modules/Services.jsm");
@@ -20,40 +15,23 @@ Cu.import("resource://gre/modules/devtools/Console.jsm");
 Cu.import("resource:///modules/gloda/index_msg.js");
 Cu.import("resource:///modules/gloda/mimemsg.js");
 
+const hostAndPort = 'localhost:7777';
+//const hostAndPort = 'dst.grierforensics.com';
+const CERT_TRUST = "C,C,C";
 var console = Components.classes["@mozilla.org/consoleservice;1"].getService(Components.interfaces.nsIConsoleService);
 
 var Greatdane = {
   /**
    * Fetches dane certs from configured dst webapp. callback will be passed an array of results
    */
-  getEmailsForCurrentMessage: function () {
-    // First we get msgHdr, a nsIMsgDbHdr
-    let mainWindow = getMail3Pane();
-    let tabmail = mainWindow.document.getElementById("tabmail");
-    // We might not always have a selected message, so check first
-    let msgHdr = mainWindow.gFolderDisplay.selectedMessage;
-
-    console.logStringMessage(msgHdr);
-
-    // The first way of examining it is through the MimeMessage representation
-    MsgHdrToMimeMessage(msgHdr, null, function (aMsgHdr, aMimeMsg) {
-      console.logStringMessage(aMsgHdr);
-    }, true, {partsOnDemand: true});
-
-    //callback(msgHdr)
-  },
-
-  getCertsForEmailAddresses: function (emailAddresses, callback) {
-    for each(var emailAddress in emailAddresses) {
-      var scrubbed = scrubEmailAddress(emailAddress);
-      ajax('GET', 'http://' + hostAndPort + '/toolset/' + encodeURIComponent(scrubbed) + '/base64', null, function (responseText) {
-        console.logStringMessage("dane lookup. email="+scrubbed+" result=" + responseText);//debug
-        let certs = JSON.parse(responseText);
-        callback && callback([{email: scrubbed, certs: certs}]);
-      }, function (responseText) {
-        console.logStringMessage("dane lookup ERROR. email="+scrubbed+" result=" + responseText);//debug
-      });
-    }
+  getCertsForEmailAddress: function (emailAddress, success, failure) {
+    ajax('GET', 'http://' + hostAndPort + '/toolset/' + encodeURIComponent(emailAddress) + '/base64', null, function (responseText) {
+      //console.logStringMessage("dane lookup. email=" + emailAddress + " result=" + responseText);//debug
+      let certs = JSON.parse(responseText);
+      success && success(certs, emailAddress);
+    }, function (responseText) {
+      failure && failure(responseText, emailAddress);
+    });
   },
 
   addCertificate: function (base64cert) {
@@ -69,26 +47,7 @@ var Greatdane = {
 };
 
 
-function scrubEmailAddress(emailAddress) {
-  return emailAddress.replace(/.*?</, "").replace(/>.*?/, "");
-}
-
-function openTab(aUrl) {
-  let tabmail = getMail3Pane().document.getElementById("tabmail");
-  tabmail.openTab("chromeTab", {chromePage: aUrl});
-}
-
-function getMail3Pane() {
-  return Cc["@mozilla.org/appshell/window-mediator;1"]
-      .getService(Ci.nsIWindowMediator)
-      .getMostRecentWindow("mail:3pane");
-}
-
 function ajax(method, url, args, onload, onerror) {
-
-  // Creating a promise
-  //var promise = new Promise(function (resolve, reject) {
-
   // Instantiates the XMLHttpRequest
   var client = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"].createInstance(Components.interfaces.nsIXMLHttpRequest);
 
@@ -113,19 +72,13 @@ function ajax(method, url, args, onload, onerror) {
   client.onload = function () {
     if (this.status >= 200 && this.status < 300) {
       // Performs the function "resolve" when this.status is equal to 2xx
-      //resolve(this.response);
       onload(this.response);
     } else {
       // Performs the function "reject" when this.status is different than 2xx
-      //reject(this.statusText);
       onerror(this.statusText);
     }
   };
   client.onerror = function () {
-    //reject(this.statusText);
     onerror(this.statusText);
   };
-  //});
-  //Return the promise
-  //return promise;
 }
